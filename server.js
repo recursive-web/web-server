249f54964e0d5849feec9d37531e89c672d40d1b
abster <rajakarunanayake@gmail.com>
2017-08-15 22:56:53 +1000

    refactored middleware to its own file

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 var express = require('express');
 var app = express();
 var port = 3000;
-
-var middleware = {
-	requireAuthentication: function(req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req, res, next) {
-		console.log('REQUEST: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-}
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
