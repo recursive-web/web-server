f72e99541aecceacce37627bad9f4dfcbb5e52cd
abster <rajakarunanayake@gmail.com>
2017-08-17 22:38:34 +1000

    updated port for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
